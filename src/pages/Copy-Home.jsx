853dc68030d714c312c47bb9c992f725674fe48a
akuridho220 <akuridho220@gmail.com>
2024-06-23 11:43:18 +0700

    update

---
+++ src/pages/Copy-Home.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import Carousel from '../components/Carousel';
+import SingleItemCarousel from '../components/SingleItemCarousel';
+
+const Home = () => {
+    const slides = [
+        "../src/assets/img/images/LobbyPendaftaran.jpg",
+        "../src/assets/img/images/RuangUtamaPelayanan2.jpg",
+    ];
+
+    const newsItems = [
+        {
+            judul: "Berita 1",
+            tanggal: "Tanggal 1",
+            deskripsi: "Ringkasan berita 1",
+            gambar: "https://via.placeholder.com/100"
+        },
+        {
+            judul: "Berita 2",
+            tanggal: "Tanggal 2",
+            deskripsi: "Ringkasan berita 2",
+            gambar: "https://via.placeholder.com/100"
+        },
+        {
+            judul: "Berita 3",
+            tanggal: "Tanggal 3",
+            deskripsi: "Ringkasan berita 3",
+            gambar: "https://via.placeholder.com/100"
+        }
+    ];
+
+    return (
+        <div>
+            <div className="relative mb-4 h-96">
+                <Carousel autoSlide={true} autoSlideInterval={3000}>
+                    {slides.map((src, index) => (
+                        <img key={index} src={src} alt={`Slide ${index + 1}`} className="w-full h-96 object-cover" />
+                    ))}
+                </Carousel>
+                <div className="absolute inset-0 flex items-center justify-center font-bold text-white text-xl gap-4 top-full transform -translate-y-1/2">
+                    <button className="bg-tertiary-900 py-16 px-12 rounded shadow-lg">Permohonan Informasi</button>
+                    <button className="bg-tertiary-900 py-16 px-12 rounded shadow-lg">Pengajuan Keberatan</button>
+                </div>
+            </div>
+
+            <div className="flex flex-row items-center justify-center font-bold gap-4 mt-28 mb-4 px-5">
+                <button className="bg-tertiary-900 text-white w-1/6 h-32 px-4 py-6 rounded">Daftar Informasi Publik</button>
+                <button className="bg-tertiary-900 text-white w-1/6 h-32 px-4 py-6 rounded">Laporan Pelayanan Informasi</button>
+                <button className="bg-tertiary-900 text-white w-1/6 h-32 px-4 py-6 rounded">Laporan Pelayanan Keberatan</button>
+                <button className="bg-tertiary-900 text-white w-1/6 h-32 px-4 py-6 rounded">Satu Data NTB</button>
+                <button className="bg-tertiary-900 text-white w-1/6 h-32 px-4 py-6 rounded">Pengaduan NTB</button>
+                <button className="bg-tertiary-900 text-white w-1/6 h-32 px-4 py-6 rounded">Data Covid 19 NTB</button>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+                <div>
+                    <h2 className="text-xl font-semibold mb-2">Berita Terbaru</h2>
+                    <SingleItemCarousel autoSlide={true} autoSlideInterval={3000}>
+                        {newsItems.map((news, index) => (
+                            <div key={index} className="flex p-4 bg-gray-200 rounded-lg">
+                                <img src={news.gambar} alt={`news-${index}`} className="w-1/3 h-24 object-cover rounded-lg mr-4" />
+                                <div>
+                                    <h3 className="text-lg font-semibold">{news.judul}</h3>
+                                    <p className="italic">{news.tanggal}</p>
+                                    <p>{news.deskripsi}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </SingleItemCarousel>
+                </div>
+                <div>
+                    <h2 className="text-xl font-semibold mb-2">Berita Populer</h2>
+                    <SingleItemCarousel autoSlide={true} autoSlideInterval={3000}>
+                        {newsItems.map((news, index) => (
+                            <div key={index} className="flex p-4 bg-gray-200 rounded-lg">
+                                <img src={news.gambar} alt={`news-${index}`} className="w-1/3 h-24 object-cover rounded-lg mr-4" />
+                                <div>
+                                    <h3 className="text-lg font-semibold">{news.judul}</h3>
+                                    <p className="italic">{news.tanggal}</p>
+                                    <p>{news.deskripsi}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </SingleItemCarousel>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
--- src/pages/si-ppid/sample-informasi.jsx
+++ src/pages/si-ppid/sample-informasi.jsx
@@ -1,7 +1,6 @@
 import { useSearchParams } from "react-router-dom";
-import BoxHeader from "../../components/BoxHeader";
 
-const SampleInformasi = ({params}) => {
+const SampleInformasi = () => {
     const [queryParameters] = useSearchParams()
     return (
         <>
@@ -27,10 +26,10 @@ const SampleInformasi = ({params}) => {
         </nav>
 
         <div className="content flex flex-col items-center m-2 md:m-4 ">
-            <BoxHeader title={queryParameters.get("judul")}/>
-            <div className="md:mx-8 lg:mx-16 xl:mx-36 py-4">
-            <p className="text-justify my-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem inventore quasi non sapiente, in vero ad consequatur ipsam deserunt voluptate labore omnis, sint esse consequuntur, eius fuga aspernatur cumque ratione molestias qui illo molestiae nulla sunt? Nemo repellendus mollitia nihil obcaecati amet adipisci, magni tenetur quibusdam sunt soluta odit? Quam quasi quia nam repellat labore. Eius voluptatibus architecto enim vitae doloremque nam aut perspiciatis autem qui vero distinctio vel tempora suscipit est, illo velit repudiandae ducimus nisi saepe natus? Perferendis consequuntur voluptatibus nulla. Beatae sapiente amet velit modi alias quas, ullam quos, libero officiis hic nesciunt eius vel inventore! Qui?</p>
-            <p className="text-justify my-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo vitae quam odit id soluta! Rem similique quas cumque nesciunt est, natus optio rerum iste autem porro dolor inventore, consequuntur non eum odio, eos maxime ex doloribus? Laboriosam, illum tempore! Iste accusamus deserunt consequatur debitis accusantium, rem ullam dignissimos, necessitatibus laboriosam ab porro exercitationem possimus impedit magnam rerum expedita perferendis iure cupiditate qui aperiam saepe. Libero, consequatur. Deserunt commodi ipsam dignissimos cupiditate earum, nam libero consequatur sed illum quaerat aspernatur quos repudiandae veniam nesciunt sapiente quis odio possimus praesentium. Sunt consequuntur inventore, dolores velit quo sint cum nesciunt reiciendis expedita facere unde corrupti earum iste ipsa repellendus. Commodi aut neque cumque doloremque nam. Repellat beatae cupiditate aliquam, quia fuga debitis illum voluptas porro blanditiis magni distinctio doloremque nesciunt in quisquam quo excepturi adipisci reiciendis? Fugiat provident natus molestias earum aut. Laudantium eligendi suscipit eaque veniam ratione ea sed voluptatem explicabo quasi!</p>
+            <div className="md:mx-8 lg:mx-16 xl:mx-36 py-4 bg-slate-100 p-8 rounded-xl">
+                <h1 className='text-2xl font-bold mb-4'>{queryParameters.get("judul")}</h1>
+                <p className="text-justify my-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem inventore quasi non sapiente, in vero ad consequatur ipsam deserunt voluptate labore omnis, sint esse consequuntur, eius fuga aspernatur cumque ratione molestias qui illo molestiae nulla sunt? Nemo repellendus mollitia nihil obcaecati amet adipisci, magni tenetur quibusdam sunt soluta odit? Quam quasi quia nam repellat labore. Eius voluptatibus architecto enim vitae doloremque nam aut perspiciatis autem qui vero distinctio vel tempora suscipit est, illo velit repudiandae ducimus nisi saepe natus? Perferendis consequuntur voluptatibus nulla. Beatae sapiente amet velit modi alias quas, ullam quos, libero officiis hic nesciunt eius vel inventore! Qui?</p>
+                <p className="text-justify my-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo vitae quam odit id soluta! Rem similique quas cumque nesciunt est, natus optio rerum iste autem porro dolor inventore, consequuntur non eum odio, eos maxime ex doloribus? Laboriosam, illum tempore! Iste accusamus deserunt consequatur debitis accusantium, rem ullam dignissimos, necessitatibus laboriosam ab porro exercitationem possimus impedit magnam rerum expedita perferendis iure cupiditate qui aperiam saepe. Libero, consequatur. Deserunt commodi ipsam dignissimos cupiditate earum, nam libero consequatur sed illum quaerat aspernatur quos repudiandae veniam nesciunt sapiente quis odio possimus praesentium. Sunt consequuntur inventore, dolores velit quo sint cum nesciunt reiciendis expedita facere unde corrupti earum iste ipsa repellendus. Commodi aut neque cumque doloremque nam. Repellat beatae cupiditate aliquam, quia fuga debitis illum voluptas porro blanditiis magni distinctio doloremque nesciunt in quisquam quo excepturi adipisci reiciendis? Fugiat provident natus molestias earum aut. Laudantium eligendi suscipit eaque veniam ratione ea sed voluptatem explicabo quasi!</p>
             </div>
         </div>
         </>
